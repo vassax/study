11f28d2bc1da391e897f06508b240669ae4918d1
vassax <vassax0806@gmail.com>
2021-06-17 22:38:46 +0300

    update 1.1

--- block1_1.py
+++ block1_1.py
@@ -5,9 +5,9 @@ print("Введите пограничное число = ")
 numberP = float(input())
 if number < numberP:
     print("Ваше число меньше пограничного")
-elif number > numberP:
-    print("Ваше число больше пограничного")
 elif number > numberP*3:
     print("Ваше число больше пограничного более, чем в 3 раза")
+elif number > numberP:
+    print("Ваше число больше пограничного")
 else:
     print("Числа равны")
