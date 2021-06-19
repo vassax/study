e61d4bc5067aece3267ddb527ab3329e1d15326c
vassax <vassax0806@gmail.com>
2021-06-19 21:53:00 +0300

    add 3.7.2

---
+++ block3_7_2.py
@@ -0,0 +1,40 @@
+# Блок 3. Задача 7.2. Про овощи на функциях
+def nameLower(name):
+    print(name.lower())
+
+
+def nameUpper(name):
+    print(name.upper())
+
+
+def nameTitle(name):
+    print(name.title())
+
+
+def outSum(name, count):
+    print('{} - {} кг.'.format(name.title(), count))
+
+
+vegetable1 = input("Введите название 1-го овоща: ")
+vegetable2 = input("Введите название 2-го овоща: ")
+vegetable3 = input("Введите название 3-го овоща: ")
+
+nameLower(vegetable1)
+nameUpper(vegetable1)
+nameTitle(vegetable1)
+
+nameLower(vegetable2)
+nameUpper(vegetable2)
+nameTitle(vegetable2)
+
+nameLower(vegetable3)
+nameUpper(vegetable3)
+nameTitle(vegetable3)
+
+countVegetable1 = input("Введите количество первого овоща: ")
+countVegetable2 = input("Введите количество второго овоща: ")
+countVegetable3 = input("Введите количество третьего овоща: ")
+
+outSum(vegetable1, countVegetable1)
+outSum(vegetable2, countVegetable2)
+outSum(vegetable3, countVegetable3)
